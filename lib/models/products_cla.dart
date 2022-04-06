aa15cbf6e16d8cee33a1e021a47582e62cf9a8ed
nermeen841 <nermeenrmadan841@gmail.com>
2022-04-07 00:30:26 +0200

    ..

--- lib/models/products_cla.dart
+++ lib/models/products_cla.dart
@@ -3,7 +3,6 @@
 import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 import 'package:html/parser.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 import 'constants.dart';
 
 class ProductCla {
