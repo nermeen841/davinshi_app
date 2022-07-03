266c56fef0b651bfa9046e627c10f46caaee9494
nermeen841 <nermeenrmadan841@gmail.com>
2022-07-04 00:04:38 +0200

    notification action

--- lib/screens/notifications/componnent/body.dart
+++ lib/screens/notifications/componnent/body.dart
@@ -1,11 +1,18 @@
 import 'package:davinshi_app/elements/newtwork_image.dart';
 import 'package:davinshi_app/lang/change_language.dart';
 import 'package:davinshi_app/models/bottomnav.dart';
+import 'package:davinshi_app/models/search_model.dart';
 import 'package:davinshi_app/provider/notification.dart';
+import 'package:davinshi_app/screens/product_info/products.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 
 import '../../../models/constants.dart';
+import '../../../models/order.dart';
+import '../../../provider/student_product.dart';
+import '../../cart/order_info.dart';
+import '../../student/student_info.dart';
+import '../../sub_categories_screen.dart';
 
 class NotificationBody extends StatefulWidget {
   const NotificationBody({Key? key}) : super(key: key);
@@ -34,72 +41,112 @@ class _NotificationBodyState extends State<NotificationBody> {
                     padding: EdgeInsets.symmetric(
                         horizontal: w * 0.02, vertical: h * 0.02),
                     itemBuilder: (context, index) {
-                      return Container(
-                        width: double.infinity,
-                        padding: EdgeInsets.symmetric(
-                            horizontal: w * 0.02, vertical: h * 0.02),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          borderRadius: BorderRadius.circular(w * 0.03),
-                          boxShadow: [
-                            BoxShadow(
-                              color: Colors.grey.withOpacity(0.5),
-                              offset: const Offset(0, 3),
-                              spreadRadius: 3,
-                              blurRadius: 3,
-                            ),
-                          ],
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          children: [
-                            (value.notificationModel!.data![index].image !=
-                                    null)
-                                ? ClipRRect(
-                                    borderRadius:
-                                        BorderRadius.circular(w * 0.03),
-                                    child: ImageeNetworkWidget(
-                                      image: value.notificationModel!
-                                          .data![index].image!,
-                                      fit: BoxFit.cover,
-                                      width: double.infinity,
-                                      height: h * 0.2,
-                                    ),
-                                  )
-                                : const SizedBox(),
-                            (value.notificationModel!.data![index].image !=
-                                    null)
-                                ? SizedBox(
-                                    height: h * 0.03,
-                                  )
-                                : const SizedBox(),
-                            Text(
-                              value.notificationModel!.data![index].title!,
-                              maxLines: 2,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontFamily: "Tajawal",
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.04),
-                            ),
-                            SizedBox(
-                              height: h * 0.015,
-                            ),
-                            Text(
-                              value.notificationModel!.data![index].body!,
-                              maxLines: 3,
-                              overflow: TextOverflow.ellipsis,
-                              style: TextStyle(
-                                  fontFamily: "Tajawal",
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w400,
-                                  fontSize:
-                                      MediaQuery.of(context).size.width * 0.04),
-                            ),
-                          ],
+                      return InkWell(
+                        onTap: () async {
+                          if (value.notificationModel!.data![index].type! ==
+                              "Order") {
+                            dialog(context);
+                            await getOrder(value
+                                    .notificationModel!.data![index].typeId!)
+                                .then((value) {
+                              if (value) {
+                                navPR(
+                                    context,
+                                    OrderInfo(
+                                      orderClass: orders[index],
+                                    ));
+                              } else {
+                                navPop(context);
+                                error(context);
+                              }
+                            });
+                          } else if (value
+                                  .notificationModel!.data![index].type! ==
+                              "Product") {
+                            navPR(
+                              context,
+                              Products(
+                                fromFav: false,
+                                productId: value
+                                    .notificationModel!.data![index].typeId!,
+                              ),
+                            );
+                          } else if (value
+                                  .notificationModel!.data![index].type! ==
+                              "Brand") {
+                          } else if (value
+                                  .notificationModel!.data![index].type! ==
+                              "Category") {}
+                        },
+                        child: Container(
+                          width: double.infinity,
+                          padding: EdgeInsets.symmetric(
+                              horizontal: w * 0.02, vertical: h * 0.02),
+                          decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(w * 0.03),
+                            boxShadow: [
+                              BoxShadow(
+                                color: Colors.grey.withOpacity(0.5),
+                                offset: const Offset(0, 3),
+                                spreadRadius: 3,
+                                blurRadius: 3,
+                              ),
+                            ],
+                          ),
+                          child: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            mainAxisAlignment: MainAxisAlignment.start,
+                            children: [
+                              (value.notificationModel!.data![index].image !=
+                                      null)
+                                  ? ClipRRect(
+                                      borderRadius:
+                                          BorderRadius.circular(w * 0.03),
+                                      child: ImageeNetworkWidget(
+                                        image: value.notificationModel!
+                                            .data![index].image!,
+                                        fit: BoxFit.cover,
+                                        width: double.infinity,
+                                        height: h * 0.2,
+                                      ),
+                                    )
+                                  : const SizedBox(),
+                              (value.notificationModel!.data![index].image !=
+                                      null)
+                                  ? SizedBox(
+                                      height: h * 0.03,
+                                    )
+                                  : const SizedBox(),
+                              Text(
+                                value.notificationModel!.data![index].title!,
+                                maxLines: 2,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontFamily: "Tajawal",
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w600,
+                                    fontSize:
+                                        MediaQuery.of(context).size.width *
+                                            0.04),
+                              ),
+                              SizedBox(
+                                height: h * 0.015,
+                              ),
+                              Text(
+                                value.notificationModel!.data![index].body!,
+                                maxLines: 3,
+                                overflow: TextOverflow.ellipsis,
+                                style: TextStyle(
+                                    fontFamily: "Tajawal",
+                                    color: Colors.black,
+                                    fontWeight: FontWeight.w400,
+                                    fontSize:
+                                        MediaQuery.of(context).size.width *
+                                            0.04),
+                              ),
+                            ],
+                          ),
                         ),
                       );
                     },
