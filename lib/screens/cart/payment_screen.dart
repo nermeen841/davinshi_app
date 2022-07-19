f9ff9e900c69aba2adf67d88b1c9c2bd78de79a9
nermeen841 <63563785+nermeen841@users.noreply.github.com>
2022-07-19 14:21:08 +0200

    ..

--- lib/screens/cart/payment_screen.dart
+++ lib/screens/cart/payment_screen.dart
@@ -73,7 +73,7 @@ class _PaymentScreenState extends State<PaymentScreen> {
           elevation: 0,
         ),
         body: MyFatoorah(
-          afterPaymentBehaviour: AfterPaymentBehaviour.AfterCallbackExecution,
+          afterPaymentBehaviour: AfterPaymentBehaviour.AfterCalbacksExecution,
           buildAppBar: (context) {
             return AppBar(
               backgroundColor: mainColor,
